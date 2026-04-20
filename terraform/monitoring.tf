2160e7f028e711b4088980f29ea8608c3070ff18
Tec Wing YONG <tec-wing@protonmail.com>
2026-04-20 10:57:25 +0200

    infra: add helm_release for kube-prometheus-stack

---
+++ terraform/monitoring.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name             = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  values = [<<EOF
+grafana:
+  adminPassword: "grafana"
+  service:
+    type: ClusterIP
+EOF
+  ]
+}
